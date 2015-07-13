e807eb1a362dce14052d2a75b5e6229856b9b0d5
Daniel Silva <daniel@developmentseed.org>
2015-07-13 18:52:40 +0100

    Add icon library version

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,5 +1,6 @@
 var $ = require('jquery');
 var _ = require('lodash');
+var version = require('../../../collecticons-lib/version.json');
 
 $(function () {
   var template = _.template($('#page-template').html());
@@ -7,6 +8,7 @@ $(function () {
   // Render icons from catalog.
   $.get('catalog.json')
     .success(function (res) {
+      res.version = version.version;
       $container.prepend(template(res));
       // Prevent clicks on icons.
       $('[data-hook="icon-link"]').click(function (e) {e.preventDefault();});
