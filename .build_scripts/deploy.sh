f414f5ca73e84882e807142edf8c6c699a06baea
Daniel da Silva <daniel@developmentseed.org>
2017-09-07 11:09:47 +0100

    Fix deploy method

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-# If this is the deploy branch, push it up to gh-pages
-if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
-  echo "Get ready, we're pushing to gh-pages!"
-  cd dist
-  git init
-  git config user.name "Travis-CI"
-  git config user.email "travis@somewhere.com"
-  git add .
-  git commit -m "CI deploy to gh-pages"
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
-else
-  echo "Not a publishable branch so we're all done here"
-fi
+echo "Deploying to gh-pages!"
+cd dist
+
+# Save some useful information
+SSH_REPO="git@github.com:${TRAVIS_REPO_SLUG}.git"
+SHA=`git rev-parse --verify HEAD`
+
+# Initialize repo
+git init
+git config user.name "Travis-CI"
+git config user.email "travis@example.com"
+git add .
+git commit -m "CI deploy to master ${SHA}"
+git show-ref
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../.build_scripts/deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+git push --force --quiet $SSH_REPO master:gh-pages
